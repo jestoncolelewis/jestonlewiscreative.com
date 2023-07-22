1deb91d319a8edcab4f188d17d7ebe5784747e8d
Jeston Lewis <jeston.lewis@protonmail.com>
2023-07-22 16:49:43 -0600

    changed fields

---
+++ lambda/form_submit.py
@@ -0,0 +1,25 @@
+import boto3
+import json
+
+ses = boto3.client("sesv2")
+
+def handler(event, context):
+    body = event["body"]
+    print(body)
+    # body = json.loads(body)
+    replyto = body["email"]
+    subject = body["subject"]
+    message = "Name: " + body["name"] + "\n" + "Message: " + body["message"]
+    response = ses.send_email(
+        FromEmailAddress = "web@jestonlewiscreative.com",
+        Destination = {"ToAddresses": ["info@jestonlewiscreative.com"]},
+        ReplyToAddresses = [replyto],
+        Content = {
+            "Simple": {
+                "Subject": {"Data": subject},
+                "Body": {"Text": {"Data": message}}
+            }
+        }
+    )
+    return response
+    
