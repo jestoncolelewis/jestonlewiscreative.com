ce3f945ee7699fcc11dc758cbc9c2bac5a4d954b
Jeston Lewis <jeston.lewis@protonmail.com>
2023-07-31 19:17:56 -0600

    changed away from pipeline for testing

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import os
 import aws_cdk as cdk
 
 from s_front_end.pipeline_stack import FrontEndPipeline
+from s_front_end.s_front_end_stack import SFrontEndStack
 
 
 app = cdk.App()
-FrontEndPipeline(app, "JestonLewisCreativeFrontEndStack", env=cdk.Environment(account="706391136734", region="us-west-2"))
+SFrontEndStack(app, "JestonLewisCreativeFrontEndStack", env=cdk.Environment(account="706391136734", region="us-west-2"))
+# FrontEndPipeline(app, "JestonLewisCreativeFrontEndStack", env=cdk.Environment(account="706391136734", region="us-west-2"))
 
 app.synth()
