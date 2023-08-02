00780812986be94539e51e4f70cc02ed191d5075
Jeston Lewis <jeston.lewis@protonmail.com>
2023-08-02 17:02:20 -0600

    added ses role policy

--- s_front_end/form.py
+++ s_front_end/form.py
@@ -1,7 +1,8 @@
 from constructs import Construct
 from aws_cdk import (
     aws_lambda as lamb,
-    aws_apigateway as api
+    aws_apigateway as api,
+    aws_iam as iam
 )
 
 class Form(Construct):
@@ -14,6 +15,13 @@ class Form(Construct):
             code=lamb.Code.from_asset("lambda"),
             handler="form_submit.handler"
         )
+        form.add_to_role_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=[
+                "ses:*"
+            ],
+            resources=["arn:aws:ses:us-west-2:706391136734:identity/jestonlewiscreative.com"] # TODO add arn dynamically check 
+        ))
 
         options = lamb.Function(
             self, "Options",
@@ -22,7 +30,6 @@ class Form(Construct):
             handler="options_return.handler"
         )
 
-        # TODO add permissions for lambda to send emails 
 
         gateway = api.LambdaRestApi(
             self, "JLCFormLambdaGateway",
